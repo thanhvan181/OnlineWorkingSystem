4bdbc89eeb597d24250995963108b0bf7496d7af
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-08-26 23:25:58 +0700

    On-9-saleman

---
+++ src/api/v1/controllers/saleman.controller.js
@@ -0,0 +1,33 @@
+const salemansService = require("../services/salemans.service");
+const catchAsync = require('../utils/catchAsync')
+module.exports = {
+    saleman: async(req, res, next) => {
+        // try {
+        //   res.status(200).send('saleman');
+        // } catch (err) {
+        //   next(err)
+        // }
+        getAllSalemans: catchAsync(async(req, res) => {
+            const salemans = await salemansService.findAll();
+            return res.status(200).json(salemans);
+        }),
+        getSaleman: catchAsync(async(req, res) => {
+            const saleman = await salemansService.findOne(req.params.id);
+            return res.status(200).json(saleman);
+        }),
+        removeSaleman: catchAsync(async(req, res) => {
+            const saleman = await salemansService.findOneAndDelete(req.params.id);
+            return res.status(200).json(saleman)
+        }),
+        updateSaleman: catchAsync(async(req, res) => {
+            const saleman = await salemansService.findOneAndUpdate(req.params.id, req.body);
+            return res.status(200).json(saleman)
+        }),
+        createSaleman: catchAsync(async(req, res) => {
+            const saleman = await salemansService.create(req.body).save();
+            return res.status(200).json(saleman)
+
+        })
+
+    },
+};
