a58be9f367ddaab6b26bd19b5a046eeb3a7cc0b3
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-08-26 23:26:10 +0700

    On-9-saleman

---
+++ src/api/v1/models/saleman.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const SalemanSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    languages: {
+        type: String,
+        required: true
+    }
+}, { collection: 'salemans', timestamps: true });
+
+module.exports = mongoose.model('salemans', SalemanSchema);
---
+++ src/api/v1/routes/saleman.router.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const salemansController = require('../controllers/saleman.controller')
+
+const router = express.Router();
+router.get('/', salemansController.getAllSalemans);
+router.get('/:id', salemansController.getSaleman);
+router.post('/', salemansController.createSaleman);
+router.delete('/:id', salemansController.removeSaleman);
+router.patch('/:id', salemansController.updateSaleman);
+module.exports = router;
