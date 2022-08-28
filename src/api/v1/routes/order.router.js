030c7d150686a5406c0992a5bc3acb25ae012fc9
dinhlcph18273 <dinhlcph18273>
2022-08-28 23:43:44 +0700

    ON-10 update validate

--- src/api/v1/routes/order.router.js
+++ src/api/v1/routes/order.router.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const orderController = require('../controllers/order.controller');
 const validate = require('../middlewares/validate.middleware');
-const orderValidatetion = require('../validations/order.validation')
+const orderValidatetion = require('../validations/order.validation');
 
 const router = express.Router();
 
-router.get('/', orderController.getAllOrders);
-router.get('/:id', orderController.getOrder);
-router.post('/',validate(orderValidatetion.createOrder) ,orderController.createOrder);
-router.put('/:id', orderController.updateOrder);
-router.delete('/:id', orderController.deleteOrder);
+router.get('/', validate(orderValidatetion.getAllOrders), orderController.getAllOrders);
+router.get('/:id', validate(orderValidatetion.getOrder), orderController.getOrder);
+router.post('/', validate(orderValidatetion.createOrder), orderController.createOrder);
+router.put('/:id', validate(orderValidatetion.updateOrder), orderController.updateOrder);
+router.delete('/:id', validate(orderValidatetion.deleteOrder), orderController.deleteOrder);
 
 module.exports = router;
