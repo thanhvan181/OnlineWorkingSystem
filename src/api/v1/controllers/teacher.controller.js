77c2311e8fa731cc7a5451753f3e1a2f9baacf93
Nguyễn Anh Quân <ngongquan@gmail.com>
2022-08-28 20:51:19 +0700

    ON-4- CRUD Teachers

--- src/api/v1/controllers/teacher.controller.js
+++ src/api/v1/controllers/teacher.controller.js
@@ -8,23 +8,23 @@ module.exports = {
   }),
   getTeacher: catchAsync(async (req, res) => {
     const filter = { _id: req.params.id };
-    const teachers = await teacherService.find(filter);
-    return res.status(200).json(teachers);
+    const teacher = await teacherService.find(filter);
+    return res.status(200).json(teacher);
   }),
   createTeacher: catchAsync(async (req, res) => {
-    const teachers = await teacherService.create(req, res);
-    return res.status(200).json(teachers);
+    const teacher = await teacherService.create(req, res);
+    return res.status(200).json(teacher);
   }),
   updateTeacher: catchAsync(async (req, res) => {
     const condition = { _id: req.params.id };
     const doc = req.body;
     const option = { new: true };
-    const teachers = await teacherService.update(condition, doc, option);
-    return res.status(200).json(teachers);
+    const teacher = await teacherService.update(condition, doc, option);
+    return res.status(200).json(teacher);
   }),
   removeTeacher: catchAsync(async (req, res) => {
     const condition = { _id: req.params.id };
-    const teachers = await teacherService.remove(condition);
-    res.status(200).json(teachers);
+    const teacher = await teacherService.remove(condition);
+    res.status(200).json(teacher);
   })
 };
--- src/api/v1/services/teacher.service.js
+++ src/api/v1/services/teacher.service.js
@@ -2,23 +2,23 @@ const _teacher = require('../models/teacher.model');
 
 module.exports = {
   findAll: async () => {
-    const users = await _teacher.find({});
-    return users;
+    const teachers = await _teacher.find({});
+    return teachers;
   },
   find: async (filter) => {
-    const users = await _teacher.findOne(filter);
-    return users;
+    const teacher = await _teacher.findOne(filter);
+    return teacher;
   },
   update: async (condition, doc, option) => {
-    const users = await _teacher.findOneAndUpdate(condition, doc, option);
-    return users;
+    const teacher = await _teacher.findOneAndUpdate(condition, doc, option);
+    return teacher;
   },
   remove: async (condition) => {
-    const users = await _teacher.findOneAndDelete(condition);
-    return users;
+    const teacher = await _teacher.findOneAndDelete(condition);
+    return teacher;
   },
   create: async (req, res) => {
-    const users = await new _teacher(req.body).save();
-    return users;
-  },
+    const teacher = await new _teacher(req.body).save();
+    return teacher;
+  }
 };
