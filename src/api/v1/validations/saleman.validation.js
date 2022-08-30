3f162351ffc2e9b6cf69959c54a8fecfd865aa0f
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-08-30 09:22:53 +0700

    ON-9-saleman

--- src/api/v1/validations/saleman.validation.js
+++ src/api/v1/validations/saleman.validation.js
@@ -1,56 +1,53 @@
 const Joi = require('joi');
 
-
 const ObjectId = (value, helper) => {
-    if (!value.match(/^[0-9a-fA-F]{24}$/)) {
-        return helper.message('{{#label}} success ! ')
-    }
-    return value;
-}
+  if (!value.match(/^[0-9a-fA-F]{24}$/)) {
+    return helper.message('{{#label}} success ! ');
+  }
+  return value;
+};
 module.exports = {
-    // getAllSalemans: {
-    //     query: Joi.object().keys({
-    //         name: Joi.string().required(),
-    //         gender: Joi.string().required(),
-    //         email: Joi.string().required(),
-    //         country: Joi.string().required(),
-    //         phone: Joi.string().required().max(10),
-    //         languages: Joi.string().required(),
-    //     })
-    // },
-    getSaleman: {
-        param: Joi.object().keys({
-            id: Joi.string().custom(ObjectId),
-        })
+  // getAllSalemans: {
+  //     query: Joi.object().keys({
+  //         name: Joi.string().required(),
+  //         gender: Joi.string().required(),
+  //         email: Joi.string().required(),
+  //         country: Joi.string().required(),
+  //         phone: Joi.string().required().max(10),
+  //         languages: Joi.string().required(),
+  //     })
+  // },
+  getSaleman: {
+    param: Joi.object().keys({
+      id: Joi.string().custom(ObjectId),
+    }),
+  },
+  createSaleman: {
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      gender: Joi.string().required(),
+      email: Joi.string().required(),
+      country: Joi.string().required(),
+      phone: Joi.string().required().max(10),
+      languages: Joi.string().required(),
+    }),
+  },
+  removeSaleman: {
+    params: Joi.object().keys({
+      id: Joi.string().custom(ObjectId),
+    }),
+  },
+  updateSaleman: {
+    params: Joi.object().keys({
+      id: Joi.string().custom(ObjectId),
+    }),
+    body: {
+      name: Joi.string().required(),
+      gender: Joi.string().required(),
+      email: Joi.string().required(),
+      country: Joi.string().required(),
+      phone: Joi.string().required().max(10),
+      languages: Joi.string().required(),
     },
-    createSaleman: {
-        body: Joi.object().keys({
-            name: Joi.string().required(),
-            gender: Joi.string().required(),
-            email: Joi.string().required(),
-            country: Joi.string().required(),
-            phone: Joi.string().required().max(10),
-            languages: Joi.string().required(),
-        })
-    },
-    removeSaleman: {
-        params: Joi.object().keys({
-            id: Joi.string().custom(ObjectId),
-        })
-    },
-    updateSaleman: {
-        params: Joi.object().keys({
-            id: Joi.string().custom(ObjectId),
-        }),
-        body: {
-            name: Joi.string().required(),
-            gender: Joi.string().required(),
-            email: Joi.string().required(),
-            country: Joi.string().required(),
-            phone: Joi.string().required().max(10),
-            languages: Joi.string().required(),
-        }
-    }
-
-
-}
+  },
+};
