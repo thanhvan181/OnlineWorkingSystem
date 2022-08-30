b9539d639019b9ea0a5d8bd0089cdd2a80c627cf
Buihuong1203 <huongbtph16610@fpt.edu.vn>
2022-08-30 09:20:48 +0700

    ON-9-saleman validation

---
+++ src/api/v1/utils/pick.js
@@ -0,0 +1,8 @@
+module.exports = (object, keys) =>
+    keys.reduce((obj, key) => {
+        if (object && Object.prototype.hasOwnProperty.call(object, key)) {
+            // eslint-disable-next-line no-param-reassign
+            obj[key] = object[key];
+        }
+        return obj;
+    }, {});
