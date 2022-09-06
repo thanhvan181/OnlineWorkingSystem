febc71f5eff73427f1cea7e143aa233a58ecb3fd
thanhvan181 <nguyenthivan18012000@gmail.com>
2022-09-06 20:51:03 +0700

    refactor re_code

--- src/api/v1/controllers/paidmanager.controller.js
+++ src/api/v1/controllers/paidmanager.controller.js
@@ -7,19 +7,19 @@ module.exports = {
     return res.status(200).json(paidmanager);
   }),
   getAll: catchAsync(async (req, res) => {
-    const paidmanagers = await paidmanagerService.getAll();
+    const paidmanagers = await paidmanagerService.findAll();
     return res.status(200).json(paidmanagers);
   }),
   get: catchAsync(async (req, res) => {
-    const paidmanager = await paidmanagerService.get(req.params.id);
+    const paidmanager = await paidmanagerService.findOne(req.params.id);
     return res.status(200).json(paidmanager);
   }),
   remove: catchAsync(async (req, res) => {
-    const paidmanager = await paidmanagerService.remove(req.params.id);
+    const paidmanager = await paidmanagerService.findOneAndDelete(req.params.id);
     return res.status(200).json(paidmanager);
   }),
   update: catchAsync(async (req, res) => {
-    const paidmanager = await paidmanagerService.update(req.params.id, req.body);
+    const paidmanager = await paidmanagerService.findOneAndUpdate(req.params.id, req.body);
     return res.status(200).json(paidmanager);
   }),
 };
