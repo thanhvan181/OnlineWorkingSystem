b815940d64410c5f1af178ee325fa75bd637ccef
lqvinh1205 <77132358+lqvinh1205@users.noreply.github.com>
2022-09-10 13:35:26 +0700

    refs # change Schema slots

--- src/api/v1/models/slot.model.js
+++ src/api/v1/models/slot.model.js
@@ -1,14 +1,25 @@
 const mongoose = require('mongoose');
 
-const SlotSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    default: ''
+const SlotSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      default: '',
+      unique: true,
+      required: true,
+    },
+    start: {
+      type: Date,
+      default: Date.now(),
+      required: true,
+    },
+    end: {
+      type: Date,
+      default: Date.now(),
+      required: true,
+    },
   },
-  value: {
-    type: Date,
-    default: Date.now()
-  }
-}, { collection: 'slots', timestamps: true });
+  { collection: 'slots', timestamps: true }
+);
 
 module.exports = mongoose.model('slots', SlotSchema);
